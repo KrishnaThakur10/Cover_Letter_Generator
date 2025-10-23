29863e9dbf03b5cb0265e9bbac05e1707a437289
Krishna Thakur <krishnathakur27082005@gmail.com>
2025-10-23 12:57:49 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,6 @@ app.use(express.json())
 
 app.use('/api/resume', routes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => (console.log(`Server is running on port ${PORT}`)
 ));
